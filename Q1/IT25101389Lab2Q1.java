85c8825e3104b66fd93bc916129366610104fa88
Ul Hassan Q.S <IT25101389@my.sliit.lk>
2026-09-19 15:36:21 +0530

    Add files via upload

---
+++ Q1/IT25101389Lab2Q1.java
@@ -0,0 +1,13 @@
+public class IT25101389Lab2Q1{
+	public static void main(String[] args){
+		
+		double perimeter = 100;
+		
+		double length = 50 * 4.0 / 7.0;
+		double width = (3.0 / 4.0) * length;
+		
+		System.out.println("Length of the fence: " + length);
+		System.out.println("Width of the fence: " + width);
+
+	}
+}
